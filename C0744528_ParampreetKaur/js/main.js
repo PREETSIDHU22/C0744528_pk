ec5f5d5ce7d3c2a9d8322a36251674e959d94b40
visualstudio <visualstudio@230-PC29>
2019-02-25 17:03:03 -0500

    array

--- C0744528_ParampreetKaur/js/main.js
+++ C0744528_ParampreetKaur/js/main.js
@@ -1,1 +1,36 @@
 ﻿// Your code here!
+var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOfspring: 5,
+    noisy: true,
+    deadly: false
+};
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOfspring: 3,
+    noisy: true,
+    deadly: true
+};
+
+var car ={
+    color:  "RED",
+    numtires: 4,
+    model: "HONDA"
+
+};
+
+var motercycle = {
+    color:  "blue",
+    numtires: 2,
+    model: "harley"
+
+};
